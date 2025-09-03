082dc09fd7294fe1caaa22afc9efedb9f8518c70
Igor Yasinsky <iyasinskyy@gmail.com>
2025-09-03 12:41:57 +0200

    add about section

---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,9 @@
+const AnimatedTitle = () => {
+  return (
+    <div className="mt-5 text-center text-4xl uppercase leading-20 md:text-8xl">
+      Disc<b>o</b>ver the world's <br /> largest shared <b>a</b>dventure
+    </div>
+  );
+};
+
+export default AnimatedTitle;
